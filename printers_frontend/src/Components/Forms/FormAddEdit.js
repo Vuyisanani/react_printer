0f076d0c85cdd6d0161cbfb2be998e0b2f39dc7b
vmeteni@gmail.com <khayalethu@pop-os.localdomain>
2020-02-27 16:38:32 +0200

    Initial commit

---
+++ printers_frontend/src/Components/Forms/FormAddEdit.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+class AddEditForm extends React.Component {
+  state = {
+    id: 0,
+    printer_name: '',
+    printer_ip: '',
+    printer_status: ''
+  }
+
+  onChange = e => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  submitFormAdd = e => {
+    e.preventDefault()
+    fetch('http://localhost:3000/crud', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        printer_name: this.state.printer_name,
+        printer_ip: this.state.printer_ip,
+        printer_status: this.state.printer_status
+      })
+    })
+      .then(response => response.json())
+      .then(item => {
+        if(Array.isArray(item)) {
+          this.props.addItemToState(item[0])
+          this.props.toggle()
+        } else {
+          console.log('failure')
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+  submitFormEdit = e => {
+    e.preventDefault()
+    fetch('http://localhost:3000/crud', {
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id: this.state.id,
+        printer_name: this.state.printer_name,
+        printer_ip: this.state.printer_ip,
+        printer_status: this.state.printer_status
+      })
+    })
+      .then(response => response.json())
+      .then(item => {
+        if(Array.isArray(item)) {
+          this.props.updateState(item[0])
+          this.props.toggle()
+        } else {
+          console.log('failure')
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+  componentDidMount(){
+    // if item exists, populate the state with proper data
+    if(this.props.item){
+      const { id, printer_name, printer_ip, printer_status } = this.props.item
+      this.setState({ id, printer_name, printer_ip, printer_status })
+    }
+  }
+
+  render() {
+    return (
+      <Form onSubmit={this.props.item ? this.submitFormEdit : this.submitFormAdd}>
+        <FormGroup>
+          <Label for="printer_name">Printer Name</Label>
+          <Input type="text" name="printer_name" id="printer_name" onChange={this.onChange} value={this.state.printer_name === null ? '' : this.state.printer_name} />
+        </FormGroup>
+        
+        <FormGroup>
+          <Label for="printer_ip">Printer IP Address</Label>
+          <Input type="text" name="printer_ip" id="printer_ip" onChange={this.onChange} value={this.state.printer_ip === null ? '' : this.state.printer_ip}  placeholder="ex. 555-555-5555" />
+        </FormGroup>
+        
+        <FormGroup>
+          <Label for="printer_status">Printer Status</Label>
+          <Input type="text" name="printer_status" id="printer_status" onChange={this.onChange} value={this.state.printer_status}  />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    );
+  }
+}
+
+export default AddEditForm
---
+++ printers_frontend/src/Components/Tables/DataTable.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import { Table, Button } from 'reactstrap';
+import ModalForm from '../Modals/Modal'
+
+class DataTable extends Component {
+
+  deleteItem = id => {
+    let confirmDelete = window.confirm('Delete Printer forever?')
+    if(confirmDelete){
+      fetch('http://localhost:3000/crud', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id
+      })
+    })
+      .then(response => response.json())
+      .then(item => {
+        this.props.deleteItemFromState(id)
+      })
+      .catch(err => console.log(err))
+    }
+
+  }
+
+  render() {
+
+    const items = this.props.items.map(item => {
+      return (
+        <tr key={item.id}>
+          <th scope="row">{item.id}</th>
+          <td>{item.printer_name}</td>
+          <td>{item.printer_ip}</td>
+          <td>{item.printer_status}</td>
+          <td>
+            <div style={{width:"110px"}}>
+              <ModalForm buttonLabel="Edit" item={item} updateState={this.props.updateState}/>
+              {' '}
+              <Button color="danger" onClick={() => this.deleteItem(item.id)}>Delete Printer</Button>
+            </div>
+          </td>
+        </tr>
+        )
+      })
+
+    return (
+      <Table responsive hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Printer Name</th>
+            <th>Printer IP Address</th>
+            <th>Printer Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items}
+        </tbody>
+      </Table>
+    )
+  }
+}
+
+export default DataTable
